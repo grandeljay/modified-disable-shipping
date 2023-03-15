cf993bcf1519b144dc170ecaac7c31ebe0249b38
grandeljay <github.jay@grandel.anonaddy.me>
2023-03-15 15:53:42 +0100

    feat(localisation): add italian language

---
+++ src/lang/italian/modules/categories/grandeljay_disable_shipping_categories.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Disable shipping
+ *
+ * @author  Jay Trees <disable-shipping@grandels.email>
+ * @link    https://github.com/grandeljay/modified-disable-shipping
+ * @package GrandelJayDisableShipping
+ */
+
+use Grandeljay\DisableShipping\Constants;
+
+$translations = array(
+    /** Module */
+    'TITLE'            => 'grandeljay - Disable shipping',
+    'LONG_DESCRIPTION' => 'Questo modulo fa parte del modulo Disattiva spedizione e dovrebbe avere lo stesso stato di attivazione.',
+    'STATUS_TITLE'     => 'Stato',
+    'STATUS_DESC'      => 'Selezioni Sì per attivare il modulo e No per disattivarlo.',
+    'TEXT_TITLE'       => 'Disable shipping',
+);
+
+foreach ($translations as $key => $value) {
+    $constant = Constants::MODULE_NAME_CATEGORIES . '_' . $key;
+
+    define($constant, $value);
+}
---
+++ src/lang/italian/modules/system/grandeljay_disable_shipping_system.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Disable shipping
+ *
+ * @author  Jay Trees <disable-shipping@grandels.email>
+ * @link    https://github.com/grandeljay/modified-disable-shipping
+ * @package GrandelJayDisableShipping
+ */
+
+use Grandeljay\DisableShipping\Constants;
+
+$translations = array(
+    /** Module */
+    'TITLE'            => 'grandeljay - Disable shipping',
+    'LONG_DESCRIPTION' => sprintf(
+        'Le permette di disattivare selettivamente i metodi di spedizione per determinati prodotti e categorie. Non dimentichi di attivare o disattivare anche le %s sotto le %s.',
+        sprintf(
+            '<a href="/' . DIR_ADMIN . 'modules.php?set=categories">%s</a>',
+            BOX_MODULE_TYPE
+        ),
+        sprintf(
+            '<ul>
+                <li>%s, und</li>
+                <li>%s.</li>
+            </ul>',
+            '<a href="/' . DIR_ADMIN . 'modules.php?set=categories&module=grandeljay_disable_shipping_categories">grandeljay_disable_shipping_categories (categories)</a>',
+            '<a href="/' . DIR_ADMIN . 'modules.php?set=checkout&module=grandeljay_disable_shipping_checkout">grandeljay_disable_shipping_checkout (checkout)</a>'
+        )
+    ),
+    'STATUS_TITLE'     => 'Stato',
+    'STATUS_DESC'      => 'Selezioni Sì per attivare il modulo e No per disattivarlo.',
+    'TEXT_TITLE'       => 'Disable shipping',
+);
+
+foreach ($translations as $key => $value) {
+    $constant = Constants::MODULE_NAME_SYSTEM . '_' . $key;
+
+    define($constant, $value);
+}
